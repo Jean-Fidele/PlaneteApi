145a38a44684dc2801a614e13e3f09907dcdfd95
Fidele <fidele999@gmail.com>
2022-10-16 19:57:33 +0200

    - Create controller file for Societein PlaneteApi

---
+++ PlaneteApi/Controllers/Societes/MetierController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlaneteApi.Controllers.Societes
+{
+    public class MetierController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PlaneteApi/Controllers/Societes/ProfessionnelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlaneteApi.Controllers.Societes
+{
+    public class ProfessionnelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
